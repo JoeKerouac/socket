10c50e4b7cc53fe8c627b9250f5e43329ce9e96f
JoeKerouac <1213812243@qq.com>
2018-05-23 15:31:18 +0800

    更改扩容操作

--- client/src/main/java/com/joe/easysocket/client/core/Reader.java
+++ client/src/main/java/com/joe/easysocket/client/core/Reader.java
@@ -185,7 +185,11 @@ public class Reader extends Worker {
         if (writeable <= threshold) {
             if (grow) {
                 //扩容四分之一后直接返回
-                buffer = CollectionUtil.grow(buffer, readPoint, writePoint - readPoint, bufferSize >> 2);
+                int size = bufferSize >> 2;
+                if (size + buffer.capacity() < 0) {
+                    size = Integer.MAX_VALUE;
+                }
+                buffer = CollectionUtil.grow(buffer, readPoint, writePoint - readPoint, size);
                 bufferSize = buffer.capacity();
                 writeable = buffer.capacity() - writePoint;
                 threshold = bufferSize >> 3;
