00236f5e16f865e1013c8aa72c38707033c57426
JoeKerouac <1213812243@qq.com>
2018-07-02 15:38:01 +0800

    更改验证参数方法

--- backserver/src/main/java/com/joe/easysocket/server/backserver/mvc/impl/resource/Resource.java
+++ backserver/src/main/java/com/joe/easysocket/server/backserver/mvc/impl/resource/Resource.java
@@ -3,17 +3,12 @@ package com.joe.easysocket.server.backserver.mvc.impl.resource;
 import com.joe.easysocket.server.backserver.mvc.Bean;
 import com.joe.easysocket.server.backserver.mvc.impl.context.HttpRequestContext;
 import com.joe.easysocket.server.backserver.mvc.impl.exception.ResourceInvokeException;
+import com.joe.utils.validator.ValidatorUtil;
 import lombok.Data;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
 import javax.validation.ValidationException;
-import javax.validation.Validator;
-import javax.validation.executable.ExecutableValidator;
 import java.lang.reflect.Method;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 /**
  * 资源接口
@@ -22,8 +17,6 @@ import java.util.Set;
  */
 @Data
 public class Resource<T> implements Bean {
-    private static final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-    private static final ExecutableValidator executableValidator = validator.forExecutables();
     // 该资源对应的实体类
     private final Class<T> resourceClass;
     // 该资源对应的方法
@@ -72,11 +65,6 @@ public class Resource<T> implements Bean {
      * @throws ValidationException 当参数不符合规定时抛出该异常
      */
     public void check(Object[] params) throws ValidationException {
-        Set<ConstraintViolation<T>> methodValidators = executableValidator.validateParameters(instance,
-                resourceMethod, params);
-        Iterator<ConstraintViolation<T>> iterator = methodValidators.iterator();
-        if (iterator.hasNext()) {
-            throw new ValidationException(iterator.next().getMessage());
-        }
+        ValidatorUtil.validateParameters(instance, resourceMethod, params);
     }
 }
